d9b14b8ae4c9342ac6d8ac71e16ff787372c4842
Nafiz43 <imtiznafiz@gmail.com>
2025-04-13 07:16:01 -0700

    minor edits in table gen code

--- 00_format_prompt_appendix_file.py
+++ 00_format_prompt_appendix_file.py
@@ -1,13 +1,12 @@
 import pandas as pd
-df = pd.read_csv('prompt-files/PCL_Questions_V5_CoT.csv')
+df = pd.read_csv('prompt-files/PCL_Questions_V5.csv')
 saving_path1 = 'data/formatted_prompts_IP.tex'
 with open(saving_path1, 'w', encoding='utf-8') as f:
     for i, row in enumerate(df['Questions'], start=1):
-        latex_block = f"""\\textit{{\\normalsize Prompt for Question  {i}:}}
-\\begin{{mdframed}}[]
-\\normalsize
+        latex_block = f"""\\texttt{{\\normalsize Prompt for Question  {i}:}}
+\\begin{{lstlisting}}[]
 {row}
-\\end{{mdframed}}\n\n"""
+\\end{{lstlisting}}\n\n"""
         f.write(latex_block)
 
 
@@ -15,11 +14,10 @@ df = pd.read_csv('prompt-files/PCL_Questions_V5_CoT.csv')
 saving_path2='data/formatted_prompts_CoT.tex'
 with open(saving_path2, 'w', encoding='utf-8') as f:
     for i, row in enumerate(df['Questions'], start=1):
-        latex_block = f"""\\textit{{\\normalsize Prompt for Question  {i}:}}
-\\begin{{mdframed}}[]
-\\normalsize
+        latex_block = f"""\\texttt{{\\normalsize Prompt for Question  {i}:}}
+\\begin{{lstlisting}}[]
 {row}
-\\end{{mdframed}}\n\n"""
+\\end{{lstlisting}}\n\n"""
         f.write(latex_block)
 
 print(f'Formatted prompts saved in {saving_path1} and {saving_path2} files')
